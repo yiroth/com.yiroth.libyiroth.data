6643dcd14b704a69f5ee01f77fdddb504af25af7
Alsiel Nokklair <alsielnokklair@gmail.com>
2025-12-12 15:24:38 +0300

    Combine Item and Node classes

--- Scripts/Node.cs
+++ Scripts/Node.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace LibYiroth.Data
 {
+    [CreateAssetMenu(fileName = "Node", menuName = "LibYiroth/Data/New Node", order = 0)]
     public class Node : ScriptableObject
     {
         public Identification nodeID;
         public string nodeName;
         public string nodeDescription;
+        public List<Variant.Container> attributeContainer;
     }
 }
